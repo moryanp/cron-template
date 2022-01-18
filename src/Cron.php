2daecf6b64b465a6843157972c663145f0053425
Natan Morya Paredes <barralagart@gmail.com>
2022-01-18 15:31:22 -0300

    created translate status feature

--- src/Cron.php
+++ src/Cron.php
@@ -5,10 +5,10 @@ namespace App;
 use App\Service\CafService;
 use App\Service\ConsultService;
 use DI\Container;
+use InvalidArgumentException;
 
 class Cron
 {
-
     /**
      * @var ConsultService
      */
@@ -18,6 +18,7 @@ class Cron
      * @var CafService
      */
     private CafService $cafService;
+
 
     public function __construct(Container $container)
     {
@@ -41,13 +42,59 @@ class Cron
             // para cada elemento do array, pesquisar seu status atual na CAF
             foreach ($listPendings as $data) {
 
-                // 1 - através do execution_id, buscar na CAF a consulta
+                // 1 - através do execution_id do atual dado, consultar na CAF a resposta
                 $execution_id = $data['cocn_execution_id'];
                 $cafResult = $this->cafService->getByExecutionId($execution_id);
 
-                // 2- se status desse dado na caf estiver diferente do dado armazenado no 
-                // bd, atualizar ele com as novas informaçoes da CAF
+                // 2- verifica se o status contido no BD diverge do status contigo na resposta da CAF
+                $status = $data['cocn_status_consulta'];
+                // $statusCaf = $this->translateStatus($cafResult['status']);
+                $statusCaf = $this->translateStatus(1);
+                if ($status != $statusCaf) {
+                    // envia os dados para serem preparados para a atualização
+                    // $this->consultService->update($data, $cafResult);
+                }
             }
+        }
+    }
+
+    /**
+     * translates the string status to a int according to its value
+     * @return int
+     */
+    public function translateStatus($status)
+    {
+        if (is_string($status)) {
+
+            $status = strtoupper($status);
+
+            switch ($status) {
+                case 'APROVADO':
+                    $status = 4;
+                    return $status;
+
+                case 'REPROVADO':
+                    $status = 3;
+                    return $status;
+
+                case 'PENDENTE OCR':
+                    $status = 2;
+                    return $status;
+
+                case 'PENDENTE':
+                    $status = 1;
+                    return $status;
+
+                case 'PROCESSANDO':
+                    $status = 0;
+                    return $status;
+
+                default:
+                    throw new InvalidArgumentException("TranslateStatus Error: cannot get properly status from CAF response");
+                    break;
+            }
+        } else {
+            throw new InvalidArgumentException("TranslateStatus Error: status must be a string");
         }
     }
 }
